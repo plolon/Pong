65f61e5160e7c22eb7e134599423026d9157449d
Dawid Bańczak <dawid.banczak99@gmail.com>
2021-10-12 22:45:35 +0200

    Created paddles logic

--- pong/Game1.cs
+++ pong/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 
 namespace pong
 {
@@ -8,7 +9,8 @@ namespace pong
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-
+        private List<Platform> players;
+        Texture2D playerT;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -19,14 +21,15 @@ namespace pong
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            players = CreatePaddles();
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            playerT = new Texture2D(GraphicsDevice, 1, 1);
+            playerT.SetData(new Color[] { Color.White });
             // TODO: use this.Content to load your game content here
         }
 
@@ -43,10 +46,23 @@ namespace pong
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
+            _spriteBatch.Begin();
             // TODO: Add your drawing code here
-
+            foreach (var player in players)
+            {
+                _spriteBatch.Draw(playerT, new Rectangle((int)player.Pos.X, (int)player.Pos.Y, 20, 100), Color.White);
+            }
+            _spriteBatch.End();
             base.Draw(gameTime);
+        }
+
+        private List<Platform> CreatePaddles()
+        {
+            return new List<Platform>()
+            {
+                new Platform(new Vector2(5, (_graphics.PreferredBackBufferHeight/2) - 50), "Player 1"),
+                new Platform(new Vector2(_graphics.PreferredBackBufferWidth-25, (_graphics.PreferredBackBufferHeight/2) - 50), "Player 2"),
+            };
         }
     }
 }
---
+++ pong/Platform.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace pong
+{
+    public class Platform
+    {
+        public Vector2 Pos { get; set; }
+        public string Name { get; set; }
+        public Platform(Vector2 startingPos, string name)
+        {
+            Pos = startingPos;
+            Name = name;
+        }
+    }
+}

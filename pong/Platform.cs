557c250bdbb974c30fec1bd1bb2b36c0cfa1eb01
Dawid Bańczak <dawid.banczak99@gmail.com>
2021-10-13 23:06:21 +0200

    Added config class and created direction switch to pos paddle

--- pong/Platform.cs
+++ pong/Platform.cs
@@ -11,5 +11,31 @@ namespace pong
             Pos = startingPos;
             Name = name;
         }
+        public void Move(Direction direction)
+        {
+            switch(direction)
+            {
+                case Direction.Up:
+                    Pos = new Vector2(Pos.X, Pos.Y-1);
+                    break;
+                case Direction.Down:
+                    Pos = new Vector2(Pos.X, Pos.Y + 1);
+                    break;
+                case Direction.Immobile:
+                    break;
+            }
+        }
+
+        private void CheckBorders()
+        {
+            if (Pos.Y < 0)
+            {
+                Pos = new Vector2(Pos.X, 0);
+            }
+            if (Pos.Y > CONFIG.HEIGHT-CONFIG.PLATFORM_SIZE)
+            {
+                Pos = new Vector2(Pos.X, CONFIG.HEIGHT - CONFIG.PLATFORM_SIZE);
+            }
+        }
     }
 }

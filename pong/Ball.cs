f9d23b4550c2a5120a3f680f4e8a1f39dbda0c54
Dawid Bańczak <dawid.banczak99@gmail.com>
2021-10-13 16:46:02 +0200

    Created ball

---
+++ pong/Ball.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pong
+{
+    public class Ball
+    {
+        public Vector2 Pos { get; set; }
+        public Ball(Vector2 startingPos)
+        {
+            Pos = startingPos;
+        }
+    }
+}

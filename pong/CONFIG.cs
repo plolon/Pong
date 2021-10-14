bcce7df27733e858377356d0d155ac7e36d825dd
Dawid Bańczak <dawid.banczak99@gmail.com>
2021-10-14 15:30:47 +0200

    Changed screen size

--- pong/CONFIG.cs
+++ pong/CONFIG.cs
@@ -2,8 +2,8 @@
 {
     public static class CONFIG
     {
-        public static int WIDTH = 720;
-        public static int HEIGHT = 480;
+        public static int WIDTH = 1080;
+        public static int HEIGHT = 720;
         public static int PLATFORM_SIZE = 100;
     }
 }

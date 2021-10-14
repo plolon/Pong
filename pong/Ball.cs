abeb984835138b5c8eb5b55de2e5dcfbdb1be274
Dawid Bańczak <dawid.banczak99@gmail.com>
2021-10-14 15:05:56 +0200

    Added new class with directions

--- pong/Ball.cs
+++ pong/Ball.cs
@@ -1,13 +1,50 @@
 ﻿using Microsoft.Xna.Framework;
+using System;
 
 namespace pong
 {
     public class Ball
     {
+        public Direction MyProperty { get; set; }
+
+        public BallDirection direction;
+
         public Vector2 Pos { get; set; }
         public Ball(Vector2 startingPos)
         {
             Pos = startingPos;
+            direction = new BallDirection();
+        }
+
+        public class BallDirection
+        {
+            public DirectionX X { get; set; }
+            public DirectionY Y { get; set; }
+            public BallDirection()
+            {
+                GenerateRandomDirections();
+            }
+
+            private void GenerateRandomDirections()
+            {
+                Random random = new Random();
+                Y = (DirectionY)random.Next(0, 2);
+                X = (DirectionX)random.Next(0, 2);
+            }
+            public enum DirectionY
+            {
+                Up,
+                Down,
+            }
+            public enum DirectionX
+            {
+                Left,
+                Right,
+            }
+
+
         }
     }
+
+
 }
